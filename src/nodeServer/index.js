4ea8abd02180b097f7670006f52abd790059fa38
Elloy Eduardo Diaz Martinez <elloy@Ricardo.local>
2017-01-25 18:03:24 -0600

    video ya conseguido, servidor de streams en camino

--- src/nodeServer/index.js
+++ src/nodeServer/index.js
@@ -1,45 +1,60 @@
 var app = require('express')();
 var http = require('http').Server(app);
-// var io = require('socket.io')(http);
-var webRTC = require('webrtc.io').listen(http);
+var io = require('socket.io')(http);
+// var webRTC = require('webrtc.io').listen(http);
 
-app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
-})
-
-http.listen(8000, function(){
-  console.log('listening on *:8000');
-})
-
-app.get('/', function(req, res){
-  res.sendfile('index.html');
-})
-
-// io.on('connection', function(socket){
-//   socket.on('chat message', function(msg){
-//     io.emit('chat message', msg);
-//   });
+// app.get('/', function(req, res){
+//   res.send('<h1>Hello world</h1>');
 // })
 
-webRTC.rtc.on('chat_msg', (data, socket) => {
-  var roomList = webRTC.rtc.rooms[data.room] || [];
-  for(var i = 0; i < roomList.length; i++){
-    if(socketId !== socket.id){
-    var soc = webRTC.rtc.getSocket(socketId);
+var users = {};
 
-    if(soc){
-      soc.send(JSON.stringify({
-          'eventName': 'receive_chat_msg',
-          'data': {
-            'messages': data.messages,
-            'color': data.color
-          }
-      }), function(error){
-        if(error){
-          console.log(error);
-        }
-      });
-    }
-  }
-  }
+http.listen(8001, function(){
+  console.log('listening on *:8001');
 })
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname +'/index.html');
+})
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    try {
+       data = JSON.parse(msg);
+       io.emit('chat message', data.message);
+    } catch (e) {
+       console.log("Invalid JSON");
+       data = {};
+    }
+  });
+
+  socket.on('video-frame', function(msg){
+    io.emit('video-frame', msg);
+  })
+
+  users[data.name] = socket;
+  socket.name = data.namew
+})
+
+// webRTC.rtc.on('chat_msg', (data, socket) => {
+//   var roomList = webRTC.rtc.rooms[data.room] || [];
+//   for(var i = 0; i < roomList.length; i++){
+//     if(socketId !== socket.id){
+//     var soc = webRTC.rtc.getSocket(socketId);
+//
+//     if(soc){
+//       soc.send(JSON.stringify({
+//           'eventName': 'receive_chat_msg',
+//           'data': {
+//             'messages': data.messages,
+//             'color': data.color
+//           }
+//       }), function(error){
+//         if(error){
+//           console.log(error);
+//         }
+//       });
+//     }
+//   }
+//   }
+// })
